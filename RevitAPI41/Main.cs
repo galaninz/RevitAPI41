f975dc1992af1e46b3510669057380f62baea169
Zakhar <zakhargalanin@gmail.com>
2022-10-14 22:43:50 -0700

    Add project files.

---
+++ RevitAPI41/Main.cs
@@ -0,0 +1,46 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Newtonsoft.Json;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI41
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            string wallInfo = string.Empty;
+
+            var walls = new FilteredElementCollector(doc)
+                .OfClass(typeof(Wall))
+                .Cast<Wall>()
+                .ToList();
+
+            foreach (Wall wall in walls)
+            {
+                wallInfo += $"{wall.Name}\t{wall.Width}{Environment.NewLine}";
+            }
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string csvPath = Path.Combine(desktopPath, "wallInfo.csv");
+
+            File.WriteAllText(csvPath, wallInfo);
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitAPI41/WallData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI41
+{
+    public class WallData
+    {
+        public string Name { get; set; }
+
+        public string Volume { get; set; }
+    }
+}
